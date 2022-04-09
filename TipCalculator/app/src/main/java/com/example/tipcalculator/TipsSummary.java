780275b8618daa3c3c91be87a331871f3bffe495
unknown <gevorgmarkarov@gmail.com>
2022-04-09 16:16:59 -0400

    Creating files

---
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/TipsSummary.java
@@ -0,0 +1,14 @@
+package com.example.tipcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipsSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips_summary);
+    }
+}
