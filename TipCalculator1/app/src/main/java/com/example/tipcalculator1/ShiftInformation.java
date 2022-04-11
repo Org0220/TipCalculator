db5df56f915a768501df65b607721ac91218452c
unknown <gevorgmarkarov@gmail.com>
2022-04-11 16:18:41 -0400

    fixed the errors

---
+++ TipCalculator1/app/src/main/java/com/example/tipcalculator1/ShiftInformation.java
@@ -0,0 +1,14 @@
+package com.example.tipcalculator1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShiftInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shift_information);
+    }
+}
---
+++ TipCalculator1/app/src/main/java/com/example/tipcalculator1/TipsSummary.java
@@ -0,0 +1,14 @@
+package com.example.tipcalculator1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipsSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips_summary);
+    }
+}
